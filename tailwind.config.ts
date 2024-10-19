a41cedbac23e1b3ba4353e5c5f4ebb3df667b3fa
Abhay Dutt <dutt.abhay@gmail.com>
2024-10-19 20:24:23 +0530

    intial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    // eslint-disable-next-line 
+    require('daisyui'),
+  ],
+} satisfies Config;
